7f8ad9e3e7e27936d360d2b045c9ee0ea4be3fdb
Jpark <web.jpark@0gmail.com>
2023-06-02 15:37:48 -0600

    update UI - FilterTodoList

--- src/components/FilterTodoList/FilterTodoList.jsx
+++ src/components/FilterTodoList/FilterTodoList.jsx
@@ -1,10 +1,12 @@
+import styles from './FilterTodoList.module.css';
+
 export default function FilterTodoList({ filters, filter, onFilterChange }) {
 	return (
-		<header>
-			<ul>
+		<header className={styles.header}>
+			<ul className={styles.filters}>
 				{filters.map((value, index) => (
 					<li key={index}>
-						<button type="button" onClick={() => onFilterChange(value)}>
+						<button type="button" className={`${styles.filter} ${filter === value ? styles.active : ''}`} onClick={() => onFilterChange(value)}>
 							{value}
 						</button>
 					</li>
