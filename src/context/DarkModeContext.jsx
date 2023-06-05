5bf1133bf3018c014322c474d84a707388c404de
Jpark <web.jpark@0gmail.com>
2023-06-05 15:26:30 -0600

    update darkmode UI

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const DarkModeContext = createContext(null);
 
@@ -9,6 +9,12 @@ export const DarkModeProvider = ({ children }) => {
 		updateDarkMode(!darkMode);
 	};
 
+	useEffect(() => {
+		const isDark = localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+		setDarkMode(isDark);
+		updateDarkMode(isDark);
+	}, []);
+
 	return <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>{children}</DarkModeContext.Provider>;
 };
 
@@ -16,6 +22,11 @@ export const useDarkMode = () => useContext(DarkModeContext);
 
 function updateDarkMode(darkMode) {
 	const element = document.documentElement.classList;
-	if (darkMode) element.add('dark');
-	else element.remove('dark');
+	if (darkMode) {
+		element.add('dark');
+		localStorage.theme = 'dark';
+	} else {
+		element.remove('dark');
+		localStorage.theme = 'light';
+	}
 }
